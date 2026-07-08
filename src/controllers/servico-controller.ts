76a2722ce231ccd81090e12b6a306e8ad3c087ae
Ytalo Santos <ytalos762@gmail.com>
2026-07-07 21:28:51 -0300

    refactoring unique search functionality (wip)

--- src/controllers/servico-controller.ts
+++ src/controllers/servico-controller.ts
@@ -5,6 +5,7 @@ import { AppError } from "../errors/app-error.js";
 import { ErrorCodes } from "../errors/error-codes.js";
 import { isServicoQueryValid, isServicoValid } from "../tools/servico-validation.js";
 import type { Prisma } from "../../generated/prisma/client.js";
+import type { servicosModel } from "../../generated/prisma/models.js";
 
 export class ServicoController {
     constructor(private readonly servicoService: ServicoService) { }
@@ -34,17 +35,15 @@ export class ServicoController {
     }
 
     get: RequestHandler = async (req, res) => {
-        const servicoData: GetServicoRequest = req.body;
-        const servidoId: string = req.params.id as string;
-        let nomeServico = servicoData ? servicoData.nome_servico : undefined;
 
-        if (!isServicoQueryValid(servicoData)) {
-            return res.status(400).json({ message: "Input inválido. Por favor, preencha o campo corretamente." })
-        }
-
+        const servidoId: string = String(req.params.id) ?? null;
+        let servico: servicosModel | servicosModel[] | null;
         try {
-            const servico = await this.servicoService.get(servidoId, nomeServico);
-            if (!servico) return res.status(404).json({ message: "Servico não encontado." });
+            if (!servidoId) {
+                servico = await this.servicoService.get();
+                return res.status(200).json({ servico });
+            }
+            servico = await this.servicoService.get(servidoId);
             return res.status(200).json({ servico });
         } catch (e) {
             if (e instanceof AppError) {
