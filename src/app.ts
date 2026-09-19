7b05e50b669bd9dbc85283786107d137a1a7a5f9
Ytalo Santos <ytalos762@gmail.com>
2026-09-18 22:34:58 -0300

    adding cors support

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { routes } from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors({
+    origin: "https://corteman-frontend.onrender.com",
+    methods: [ "GET", "POST", "PUT", "DELETE"]
+}));
 
 routes(app);
 export { app };
